c5163a71dca4eb8152d6fd13abc48f375710e034
honeyzwane <112810387+honeyzwane@users.noreply.github.com>
2023-03-20 15:45:14 +0200

    update

--- eda.py
+++ eda.py
@@ -1,24 +1,50 @@
-<<<<<<< HEAD
 import pandas as pd
 import warnings
+import matplotlib.pyplot as plt
+
+#A simple code to analyse the dataset
+
+import pandas as pd
+import warnings
+
+warnings.filterwarnings('ignore')
 import matplotlib.pyplot as plt
 
 
 def EDA(data):
     """Perform exploratory data analysis on a given dataset.
-
     Args:
       data (pandas.DataFrame): The dataset to analyze.
     """
+    from IPython.display import display
+    display(data.head())
+
+    # Print the shape of the dataset
+    print(f"Number of rows: {data.shape[0]}")
+    print(f"Number of columns: {data.shape[1]}")
+
     # Check for missing values
     if data.isnull().values.any():
         # Find the number of missing values for each column
         missing_counts = data.isnull().sum()
+        # Calculate the percentage of missing values for each column
+        missing_percentages = 100 * missing_counts / len(data)
         # Select only the columns with missing values
-        missing_columns = missing_counts[missing_counts > 0]
-        print(f"Missing values found in columns: {missing_columns}")
+        missing_columns = missing_percentages[missing_percentages > 0]
+        print(f"Percentage of missing values in each column:")
+        print(missing_columns)
     else:
         print("No missing values found")
+
+    # Check for duplicated rows
+    duplicates = data[data.duplicated()]
+    if duplicates.empty:
+        print("No duplicated rows found")
+    else:
+        duplicate_count = len(duplicates)
+        total_count = len(data)
+        print(
+            f"{duplicate_count} out of {total_count} rows are duplicated ({100 * duplicate_count / total_count:.2f}%)")
 
     # Check for outliers
     q1 = data.quantile(0.25)
@@ -26,86 +52,18 @@ def EDA(data):
     iqr = q3 - q1
     lower_bound = q1 - (1.5 * iqr)
     upper_bound = q3 + (1.5 * iqr)
-    outliers = data[(data < lower_bound) | (data > upper_bound)]
-    if outliers.empty:
+    outliers = ((data < lower_bound) | (data > upper_bound)).sum()
+    if outliers.sum() == 0:
         print("No outliers found")
     else:
-        print(f"Outliers found at positions: {outliers.index}")
+        print(f"Number of outliers per column:")
+        print(outliers)
 
     # Check data types
     data_types = data.dtypes
     print(f"Data types: {data_types}")
 
-    # Check value counts for relevant columns
-    relevant_values = ['yes', 'Yes', 'y', 'Y', 'no', 'No', 'n', 'N', 'Boy', 'Girl','boy','girl','g','b','G','B', 'Male', 'male', 'm', 'M', 'Female', 'female', 'f',
-                       'F']
-    for column in data.columns:
-        if any(value in data[column].unique() for value in relevant_values):
-            counts = data[column].value_counts()
-
     # Check correlations
     correlations = data.corr()
     print("Correlation table:")
     print(correlations)
-
-    # Check for skewness
-    skewness = data.skew()
-    print("Skewness:")
-    skewness.plot(kind='bar')
-    plt.show()
-=======
-import pandas as pd
-import warnings
-import matplotlib.pyplot as plt
-
-
-def EDA(data):
-    """Perform exploratory data analysis on a given dataset.
-
-    Args:
-      data (pandas.DataFrame): The dataset to analyze.
-    """
-    # Check for missing values
-    if data.isnull().values.any():
-        # Find the number of missing values for each column
-        missing_counts = data.isnull().sum()
-        # Select only the columns with missing values
-        missing_columns = missing_counts[missing_counts > 0]
-        print(f"Missing values found in columns: {missing_columns}")
-    else:
-        print("No missing values found")
-
-    # Check for outliers
-    q1 = data.quantile(0.25)
-    q3 = data.quantile(0.75)
-    iqr = q3 - q1
-    lower_bound = q1 - (1.5 * iqr)
-    upper_bound = q3 + (1.5 * iqr)
-    outliers = data[(data < lower_bound) | (data > upper_bound)]
-    if outliers.empty:
-        print("No outliers found")
-    else:
-        print(f"Outliers found at positions: {outliers.index}")
-
-    # Check data types
-    data_types = data.dtypes
-    print(f"Data types: {data_types}")
-
-    # Check value counts for relevant columns
-    relevant_values = ['yes', 'Yes', 'y', 'Y', 'no', 'No', 'n', 'N', 'Boy', 'Girl','boy','girl','g','b','G','B', 'Male', 'male', 'm', 'M', 'Female', 'female', 'f',
-                       'F']
-    for column in data.columns:
-        if any(value in data[column].unique() for value in relevant_values):
-            counts = data[column].value_counts()
-
-    # Check correlations
-    correlations = data.corr()
-    print("Correlation table:")
-    print(correlations)
-
-    # Check for skewness
-    skewness = data.skew()
-    print("Skewness:")
-    skewness.plot(kind='bar')
-    plt.show()
->>>>>>> 9a926d635a4e8c56ba8dc40f6ee178c59df63b95
